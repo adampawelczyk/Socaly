6ced2b4210a1a76e1e0abd8220ad8d38b3762b65
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-08 20:56:46 +0200

    Create generateVerificationToken method

--- src/main/java/com/socaly/service/AuthService.java
+++ src/main/java/com/socaly/service/AuthService.java
@@ -2,19 +2,23 @@ package com.socaly.service;
 
 import com.socaly.dto.RegisterRequest;
 import com.socaly.entity.User;
+import com.socaly.entity.VerificationToken;
 import com.socaly.repository.UserRepository;
+import com.socaly.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -26,5 +30,18 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+        
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
     }
 }
