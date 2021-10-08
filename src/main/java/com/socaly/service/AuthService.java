984afc9c9299f9dfe3bb9eddd734dc225e4af6e6
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-08 20:44:26 +0200

    Create a class containing the main business logic for user registration

---
+++ src/main/java/com/socaly/service/AuthService.java
@@ -0,0 +1,30 @@
+package com.socaly.service;
+
+import com.socaly.dto.RegisterRequest;
+import com.socaly.entity.User;
+import com.socaly.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+    }
+}
