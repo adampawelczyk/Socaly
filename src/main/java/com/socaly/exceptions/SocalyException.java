9c5f94f9af9c7c63058c7e10e454e679bf63146b
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-09 11:55:53 +0200

    Create SocalyException class

---
+++ src/main/java/com/socaly/exceptions/SocalyException.java
@@ -0,0 +1,7 @@
+package com.socaly.exceptions;
+
+public class SocalyException extends RuntimeException {
+    public SocalyException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}
