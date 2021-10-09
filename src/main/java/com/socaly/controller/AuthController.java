d39aad6d3983ebcf6e1aeadf4ef75299fc467d1f
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-09 12:44:08 +0200

    Create instance of AuthService and call the signup method

--- src/main/java/com/socaly/controller/AuthController.java
+++ src/main/java/com/socaly/controller/AuthController.java
@@ -1,14 +1,25 @@
 package com.socaly.controller;
 
 import com.socaly.dto.RegisterRequest;
+import com.socaly.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService authService;
+
     @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest) {}
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+
+        return new ResponseEntity<>("User registration successful", HttpStatus.OK);
+    }
 }
