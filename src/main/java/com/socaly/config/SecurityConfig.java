73085e866508db334bb85e2607b134eef51804e6
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-10 22:43:39 +0200

    Create CommunityService class

--- src/main/java/com/socaly/config/SecurityConfig.java
+++ src/main/java/com/socaly/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.socaly.config;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -26,7 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().authorizeRequests().antMatchers("/api/auth/**").permitAll()
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/community").permitAll()
                 .anyRequest().authenticated();
     }
 
