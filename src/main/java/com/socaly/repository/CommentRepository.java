b66182ad9a66f9aa83cbc1800072c00dd05147d9
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-13 23:07:27 +0200

    Add findByPost and findByUser methods in CommentRepository

--- src/main/java/com/socaly/repository/CommentRepository.java
+++ src/main/java/com/socaly/repository/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.socaly.repository;
 
 import com.socaly.entity.Comment;
+import com.socaly.entity.Post;
+import com.socaly.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+    List<Comment> findByUser(User user);
 }
