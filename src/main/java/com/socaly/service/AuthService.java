e7949b6fab0994b48013dd5108fdce5c0307d531
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-13 13:34:35 +0200

    Add a getCurrentUser method to get current logged user

--- src/main/java/com/socaly/service/AuthService.java
+++ src/main/java/com/socaly/service/AuthService.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -66,6 +67,19 @@ public class AuthService {
 
         verificationToken.orElseThrow(() -> new SocalyException("Invalid token"));
         fetchUserAndEnable(verificationToken.get());
+    }
+
+    @Transactional(readOnly = true)
+    public User getCurrentUser() {
+        org.springframework.security.core.userdetails.User principal =
+                (org.springframework.security.core.userdetails.User) SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getPrincipal();
+
+        return userRepository.findByUsername(principal.getUsername()).orElseThrow(
+                () -> new UsernameNotFoundException("User name not found - " + principal.getUsername())
+        );
     }
 
     @Transactional
