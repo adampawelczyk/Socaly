cd8c218fd13a8bde68c2ab1db90b078ed792a99e
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-13 23:11:06 +0200

    Create CommentMapper interface

---
+++ src/main/java/com/socaly/mapper/CommentMapper.java
@@ -0,0 +1,21 @@
+package com.socaly.mapper;
+
+import com.socaly.dto.CommentDto;
+import com.socaly.entity.Comment;
+import com.socaly.entity.Post;
+import com.socaly.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    Comment map(CommentDto commentDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentDto mapToDto(Comment comment);
+}
