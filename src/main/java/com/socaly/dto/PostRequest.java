de1459a6e97d35c1e67320a20e9b23e45caeda25
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-13 13:36:33 +0200

    Create PostRequest class

---
+++ src/main/java/com/socaly/dto/PostRequest.java
@@ -0,0 +1,16 @@
+package com.socaly.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRequest {
+    private Long postId;
+    private String communityName;
+    private String postName;
+    private String url;
+    private String description;
+}
