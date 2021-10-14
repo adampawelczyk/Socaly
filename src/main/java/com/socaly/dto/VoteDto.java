4f631d3f973c4e462330e3a8afb3c1aaaa9feef2
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-14 22:53:41 +0200

    Create VoteDto class

---
+++ src/main/java/com/socaly/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.socaly.dto;
+
+import com.socaly.entity.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
