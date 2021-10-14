0c64656bc8f1f5a7af3d260251a79cc6d49ad7a7
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-14 22:51:43 +0200

    Change PostMapper from interface to abstract class and include newly added fields in PostResponse

--- src/main/java/com/socaly/mapper/PostMapper.java
+++ src/main/java/com/socaly/mapper/PostMapper.java
@@ -1,22 +1,41 @@
 package com.socaly.mapper;
 
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.socaly.dto.PostRequest;
 import com.socaly.dto.PostResponse;
 import com.socaly.entity.Community;
 import com.socaly.entity.Post;
 import com.socaly.entity.User;
+import com.socaly.repository.CommentRepository;
+import com.socaly.repository.VoteRepository;
+import com.socaly.service.AuthService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+    @Autowired
+    private CommentRepository commentRepository;
+
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "description", source = "postRequest.description")
     @Mapping(target = "user", source = "user")
-    Post map(PostRequest postRequest, Community community, User user);
+    @Mapping(target = "voteCount", constant = "0")
+    public abstract Post map(PostRequest postRequest, Community community, User user);
 
     @Mapping(target = "id", source = "postId")
     @Mapping(target = "communityName", source = "community.name")
     @Mapping(target = "userName", source = "user.username")
-    PostResponse mapToDto(Post post);
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
