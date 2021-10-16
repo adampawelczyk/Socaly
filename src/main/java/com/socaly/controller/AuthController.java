924a9227ecc5b369ceba47f0f7bf8665ef663d93
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-10-16 20:26:17 +0200

    Add post mapping to refresh token and to logout

--- src/main/java/com/socaly/controller/AuthController.java
+++ src/main/java/com/socaly/controller/AuthController.java
@@ -2,18 +2,23 @@ package com.socaly.controller;
 
 import com.socaly.dto.AuthenticationResponse;
 import com.socaly.dto.LoginRequest;
+import com.socaly.dto.RefreshTokenRequest;
 import com.socaly.dto.RegisterRequest;
 import com.socaly.service.AuthService;
+import com.socaly.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
@@ -32,5 +37,17 @@ public class AuthController {
     @PostMapping("/login")
     public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
+    }
+
+    @PostMapping("refresh/token")
+    public AuthenticationResponse refreshToken(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Refresh token deleted successfully");
     }
 }
