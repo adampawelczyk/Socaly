3cc6acc450053e00b86f1465be26291481535cac
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-12-13 21:22:14 +0100

    add mapping for createdDate and user

--- src/main/java/com/socaly/mapper/CommunityMapper.java
+++ src/main/java/com/socaly/mapper/CommunityMapper.java
@@ -3,6 +3,7 @@ package com.socaly.mapper;
 import com.socaly.dto.CommunityDto;
 import com.socaly.entity.Community;
 import com.socaly.entity.Post;
+import com.socaly.entity.User;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -20,5 +21,7 @@ public interface CommunityMapper {
 
     @InheritInverseConfiguration
     @Mapping(target = "posts", ignore = true)
-    Community mapDtoToCommunity(CommunityDto communityDto);
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "user", source = "user")
+    Community mapDtoToCommunity(CommunityDto communityDto, User user);
 }
