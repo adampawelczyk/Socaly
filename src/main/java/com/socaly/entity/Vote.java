7d8c1d1ef260b0ea99fee72cdd3b69cc97d62f4f
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-12-18 23:47:31 +0100

    change id column name from voteId to id

--- src/main/java/com/socaly/entity/Vote.java
+++ src/main/java/com/socaly/entity/Vote.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.NotNull;
 public class Vote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long voteId;
+    private Long id;
 
     private VoteType voteType;
 
