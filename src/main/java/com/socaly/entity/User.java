467e9e3d7bd81cc1c35920d21826fe84c667b155
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-12-18 23:48:47 +0100

    change id column name from userId to id

--- src/main/java/com/socaly/entity/User.java
+++ src/main/java/com/socaly/entity/User.java
@@ -20,7 +20,7 @@ import java.time.Instant;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long id;
 
     @NotBlank(message = "Username is required")
     private String username;
