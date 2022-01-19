12adfbff49a28cd0111ef0ca4fd89bf40ab730a9
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2022-01-19 23:31:41 +0100

    add list of images to the post

--- src/main/java/com/socaly/entity/Post.java
+++ src/main/java/com/socaly/entity/Post.java
@@ -9,6 +9,7 @@ import org.springframework.lang.Nullable;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
+import java.util.List;
 
 @Data
 @Entity
@@ -29,6 +30,9 @@ public class Post {
 
     private Integer voteCount = 0;
     private Instant createdDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Image> images;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
