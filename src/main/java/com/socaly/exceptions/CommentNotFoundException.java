3f6f29c2bcc7da0c044832e33eeb3a5ebc064fe4
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2022-01-31 21:25:22 +0100

    create CommentNotFoundException class

---
+++ src/main/java/com/socaly/exceptions/CommentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.socaly.exceptions;
+
+public class CommentNotFoundException extends RuntimeException {
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+}
