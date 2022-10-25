8f0c2f3d609ebfd63f3a17e7706d72b3fc71190d
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2022-10-25 23:52:06 +0200

    Rename the target from createdDate to creationDate

--- src/main/java/com/socaly/mapper/CommentMapper.java
+++ src/main/java/com/socaly/mapper/CommentMapper.java
@@ -22,7 +22,7 @@ public abstract class CommentMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "text", source = "commentDto.text")
     @Mapping(target = "voteCount", constant = "0")
-    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "creationDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "post", source = "post")
     @Mapping(target = "user", source = "user")
     public abstract Comment map(CommentDto commentDto, Post post, User user);
