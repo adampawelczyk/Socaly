cbd5169eb7ca27ac0cce38293dbeb463fe90ea16
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-02-21 23:25:47 +0100

    Create UserController class

---
+++ src/main/java/com/socaly/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.socaly.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+}
