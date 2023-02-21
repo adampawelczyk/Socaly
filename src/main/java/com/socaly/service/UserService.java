6abb89b52933bb6f10225324df521ecff7b1fb1e
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-02-21 23:25:05 +0100

    Add getUserDetails method

--- src/main/java/com/socaly/service/UserService.java
+++ src/main/java/com/socaly/service/UserService.java
@@ -1,10 +1,24 @@
 package com.socaly.service;
 
+import com.socaly.dto.UserDto;
+import com.socaly.mapper.UserMapper;
+import com.socaly.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
+    public UserDto getUserDetails(String username) {
+        return userRepository.findByUsername(username)
+                .stream().map(userMapper::mapToDto)
+                .findFirst()
+                .orElseThrow(
+                    () -> new UsernameNotFoundException(username)
+                );
+    }
 }
