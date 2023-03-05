616c16eb4ae0f7ef9cebc01944f9a1c80dd3859b
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-05 20:00:19 +0100

    Delete public access modifier from LogInRequest class

--- src/main/java/com/socaly/auth/LogInRequest.java
+++ src/main/java/com/socaly/auth/LogInRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LogInRequest {
+class LogInRequest {
     private String username;
     private String password;
 }
