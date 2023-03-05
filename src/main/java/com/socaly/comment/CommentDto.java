23ba0e6f3cdd24c4b47c7abfa6743a885c1a000a
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-05 20:30:27 +0100

    Delete public access modifier from CommentDto class

--- src/main/java/com/socaly/comment/CommentDto.java
+++ src/main/java/com/socaly/comment/CommentDto.java
@@ -9,7 +9,7 @@ import java.time.Instant;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CommentDto {
+class CommentDto {
     private Long id;
     private Long postId;
     private Long parentCommentId;
