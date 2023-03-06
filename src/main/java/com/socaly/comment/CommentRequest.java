81d9d7b4a46ddb259416b177c8d6a298d225f47b
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-06 21:48:46 +0100

    Delete unused fields of CommentRequest class

--- src/main/java/com/socaly/comment/CommentRequest.java
+++ src/main/java/com/socaly/comment/CommentRequest.java
@@ -4,16 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 class CommentRequest {
-    private Long id;
     private Long postId;
-    private Long parentCommentId;
-    private Instant createdDate;
     private String text;
-    private String username;
+    private Long parentCommentId;
 }
