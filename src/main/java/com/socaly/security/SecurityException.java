58a50b85e28b74f51b83e680b387642ab7a2b9d9
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-12 22:19:07 +0100

    Create SecurityException class

---
+++ src/main/java/com/socaly/security/SecurityException.java
@@ -0,0 +1,7 @@
+package com.socaly.security;
+
+public class SecurityException extends RuntimeException {
+    public SecurityException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}
