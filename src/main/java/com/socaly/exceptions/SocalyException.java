6bf13619b9cda99bc5682ba0b2dfc694e915fc65
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-14 22:54:48 +0100

    Delete SocalyException class

--- src/main/java/com/socaly/exceptions/SocalyException.java
+++
@@ -1,7 +0,0 @@
-package com.socaly.exceptions;
-
-public class SocalyException extends RuntimeException {
-    public SocalyException(String exceptionMessage) {
-        super(exceptionMessage);
-    }
-}
