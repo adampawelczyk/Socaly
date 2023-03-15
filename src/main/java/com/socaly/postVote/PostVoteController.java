907c4f3f1c9c24ec21a4c7334030efc3189437e3
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-15 23:22:50 +0100

    Rename VoteController class to PostVoteController

--- src/main/java/com/socaly/postVote/PostVoteController.java
+++ src/main/java/com/socaly/postVote/PostVoteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/votes/")
-public class VoteController {
+public class PostVoteController {
     private final VoteService voteService;
 
     @PostMapping
