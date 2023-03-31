8f1431266dc806c852d1c7e7c8e2f51d5826c259
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-03-31 23:13:53 +0200

    Create UserSettingsResponse class

---
+++ src/main/java/com/socaly/user/UserSettingsResponse.java
@@ -0,0 +1,17 @@
+package com.socaly.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSettingsResponse {
+    private String createdDate;
+    private String profileImage;
+    private String profileBanner;
+    private String description;
+    private String email;
+    private boolean isEmailVerified;
+}
