6f4c9804371780cbf4975ffb18df857800a38d6f
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-05 23:39:34 +0200

    Create an endpoint to get the settings of the currently logged-in user

--- src/main/java/com/socaly/userSettings/UserSettingsController.java
+++ src/main/java/com/socaly/userSettings/UserSettingsController.java
@@ -1,6 +1,7 @@
 package com.socaly.userSettings;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/api/user/settings")
 public class UserSettingsController {
+    private final UserSettingsService userSettingsService;
 
+    @GetMapping("/get")
+    UserSettingsResponse getCurrentUserSettings() {
+        return userSettingsService.getCurrentUserSettings();
+    }
 }
