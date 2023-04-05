ec6319d005a4985c8942d3a095b817b841768434
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-05 23:39:05 +0200

    Create a method to get the settings of the currently logged-in user

--- src/main/java/com/socaly/userSettings/UserSettingsService.java
+++ src/main/java/com/socaly/userSettings/UserSettingsService.java
@@ -1,9 +1,29 @@
 package com.socaly.userSettings;
 
+import com.socaly.auth.AuthService;
+import com.socaly.user.User;
+import com.socaly.user.UserMapper;
+import com.socaly.user.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserSettingsService {
+    private final AuthService authService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserSettingsResponse getCurrentUserSettings() {
+        User currentUser = authService.getCurrentUser();
+
+        return userRepository.findByUsername(currentUser.getUsername())
+                .stream()
+                .map(userMapper::mapToUserSettings)
+                .findFirst()
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(currentUser.getUsername())
+                );
+    }
 }
