af9759ce8acbae0d803b9119f431a716045172ca
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-05 23:29:35 +0200

    Create UserSettingsController class

---
+++ src/main/java/com/socaly/userSettings/UserSettingsController.java
@@ -0,0 +1,12 @@
+package com.socaly.userSettings;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user/settings")
+public class UserSettingsController {
+
+}
