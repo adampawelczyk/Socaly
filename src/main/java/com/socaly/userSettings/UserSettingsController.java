c5dd93a8f6d65a295f0275edb11b4e6fd359362c
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-07 00:13:47 +0200

    Create an endpoint to change the communityContentSort setting

--- src/main/java/com/socaly/userSettings/UserSettingsController.java
+++ src/main/java/com/socaly/userSettings/UserSettingsController.java
@@ -1,9 +1,10 @@
 package com.socaly.userSettings;
 
+import com.socaly.util.Sorting;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -14,5 +15,11 @@ public class UserSettingsController {
     @GetMapping("/get")
     public UserSettingsResponse getCurrentUserSettings() {
         return userSettingsService.getCurrentUserSettings();
+    }
+
+    @PatchMapping("/change/community-content-sort")
+    public ResponseEntity<Void> changeCommunityContentSort(@RequestBody Sorting sorting) {
+        userSettingsService.changeCommunityContentSort(sorting);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
