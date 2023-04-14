fdeebbeec999b95cf543682e2a21a4f4158bc15f
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-14 22:59:59 +0200

    Create UserCommunitySettingsRepository interface

---
+++ src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsRepository.java
@@ -0,0 +1,7 @@
+package com.socaly.userCommunitySettings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCommunitySettingsRepository extends JpaRepository<UserCommunitySettings, Long> {
+
+}
