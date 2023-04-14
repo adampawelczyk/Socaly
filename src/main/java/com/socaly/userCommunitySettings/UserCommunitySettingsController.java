7fd53ca2a4ad185bc0c3762aefbdcf8b25708918
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-14 23:05:23 +0200

    Create UserCommunitySettingsController class

---
+++ src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsController.java
@@ -0,0 +1,11 @@
+package com.socaly.userCommunitySettings;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user/community/settings")
+public class UserCommunitySettingsController {
+
+}
