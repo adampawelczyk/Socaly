26e5f2ea57597c1c95f6e6f3e534b3af988467d0
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-14 23:02:50 +0200

    Create getUserCommunitySettingsById method

--- src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsService.java
+++ src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsService.java
@@ -1,10 +1,26 @@
 package com.socaly.userCommunitySettings;
 
+import com.socaly.auth.AuthService;
+import com.socaly.user.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
 public class UserCommunitySettingsService {
+    private final AuthService authService;
+    private final UserCommunitySettingsMapper userCommunitySettingsMapper;
 
+    UserCommunitySettingsResponse getUserCommunitySettingsById(Long communityId) {
+        User currentUser = authService.getCurrentUser();
+
+        return currentUser.getUserCommunitySettings()
+                .stream()
+                .filter(settings -> Objects.equals(settings.getCommunityId(), communityId))
+                .map(userCommunitySettingsMapper::mapToUserCommunitySettingsResponse)
+                .findFirst()
+                .orElseThrow(() -> new UserCommunitySettingsNotFoundException(currentUser.getUsername()));
+    }
 }
