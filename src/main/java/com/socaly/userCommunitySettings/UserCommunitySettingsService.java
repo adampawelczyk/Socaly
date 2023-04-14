881c6309307800bd4e0a7de5ae8086952b846737
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-14 23:03:45 +0200

    Create changeCommunityContentSort method

--- src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsService.java
+++ src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsService.java
@@ -2,6 +2,7 @@ package com.socaly.userCommunitySettings;
 
 import com.socaly.auth.AuthService;
 import com.socaly.user.User;
+import com.socaly.util.Sorting;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.Objects;
 public class UserCommunitySettingsService {
     private final AuthService authService;
     private final UserCommunitySettingsMapper userCommunitySettingsMapper;
+    private final UserCommunitySettingsRepository userCommunitySettingsRepository;
 
     UserCommunitySettingsResponse getUserCommunitySettingsById(Long communityId) {
         User currentUser = authService.getCurrentUser();
@@ -22,5 +24,18 @@ public class UserCommunitySettingsService {
                 .map(userCommunitySettingsMapper::mapToUserCommunitySettingsResponse)
                 .findFirst()
                 .orElseThrow(() -> new UserCommunitySettingsNotFoundException(currentUser.getUsername()));
+    }
+
+    void changeCommunityContentSort(Long communityId, Sorting sorting) {
+        User currentUser = authService.getCurrentUser();
+
+        UserCommunitySettings userCommunitySettings =currentUser.getUserCommunitySettings()
+                .stream()
+                .filter(settings -> Objects.equals(settings.getCommunityId(), communityId))
+                .findFirst()
+                .orElseThrow(() -> new UserCommunitySettingsNotFoundException(currentUser.getUsername()));
+
+        userCommunitySettings.setCommunityContentSort(sorting);
+        userCommunitySettingsRepository.save(userCommunitySettings);
     }
 }
