403213ab5ca26c03487c1c2e883d7276bd1c8328
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-15 22:49:35 +0200

    Create a get endpoint to get userCommunitySettings by communityId

--- src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsController.java
+++ src/main/java/com/socaly/userCommunitySettings/UserCommunitySettingsController.java
@@ -7,5 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RequestMapping("/api/user/community/settings")
 public class UserCommunitySettingsController {
+    private final UserCommunitySettingsService userCommunitySettingsService;
 
+    @GetMapping("/get/{communityId}")
+    public UserCommunitySettingsResponse getUserCommunitySettingsById(@PathVariable Long communityId) {
+        return userCommunitySettingsService.getUserCommunitySettingsById(communityId);
+    }
 }
