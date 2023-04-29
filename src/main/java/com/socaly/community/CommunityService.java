7dc2f388fb7f598340c118500b6e75f446a34b8e
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-04-29 21:39:12 +0200

    When a user leaves a community, delete the userCommunitySettings for that community

--- src/main/java/com/socaly/community/CommunityService.java
+++ src/main/java/com/socaly/community/CommunityService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -75,6 +76,18 @@ public class CommunityService {
 
         community.getUsers().remove(currentUser);
         communityRepository.save(community);
+
+        List<UserCommunitySettings> userCommunitySettingsList = currentUser.getUserCommunitySettings();
+        UserCommunitySettings userCommunitySettings = userCommunitySettingsList
+                .stream()
+                .filter(settings -> Objects.equals(settings.getCommunityId(), community.getId()))
+                .findFirst()
+                .orElseThrow(() -> new UserCommunitySettingsNotFoundException(currentUser.getUsername()));
+
+        currentUser.getUserCommunitySettings().remove(userCommunitySettings);
+        userRepository.save(currentUser);
+
+        userCommunitySettingsRepository.delete(userCommunitySettings);
     }
 
     public List<CommunityResponse> getAllCommunitiesForUser(String name) {
