c536db3eef342980f2693e34aeadf0a9572885df
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-05-23 18:18:25 +0200

    Delete the body field and add the emailAddress, username and verificationLink fields.

--- src/main/java/com/socaly/email/EmailVerificationEmail.java
+++ src/main/java/com/socaly/email/EmailVerificationEmail.java
@@ -10,5 +10,7 @@ import lombok.NoArgsConstructor;
 public class EmailVerificationEmail {
     private String subject;
     private String recipient;
-    private String body;
+    private String emailAddress;
+    private String username;
+    private String verificationLink;
 }
