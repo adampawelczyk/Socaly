043ec894692940e8923f451fcfa72cd5f9154789
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-05-29 22:18:19 +0200

    Create the CommentReplyEmail class

---
+++ src/main/java/com/socaly/email/CommentReplyEmail.java
@@ -0,0 +1,23 @@
+package com.socaly.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentReplyEmail {
+    private String subject;
+    private String recipient;
+    private String username;
+    private String profileImage;
+    private String replyingUsername;
+    private String replyingUserProfileImage;
+    private String postTitle;
+    private String postTimestamp;
+    private String communityName;
+    private String commentText;
+    private String commentTimestamp;
+    private String replyText;
+}
