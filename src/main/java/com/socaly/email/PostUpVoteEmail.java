14b9233ca1fed4669f3161bbb84ab3122d174ec4
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-05-30 21:35:43 +0200

    Create the PostUpVoteEmail class

---
+++ src/main/java/com/socaly/email/PostUpVoteEmail.java
@@ -0,0 +1,21 @@
+package com.socaly.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostUpVoteEmail {
+    private String subject;
+    private String recipient;
+    private String username;
+    private String profileImage;
+    private String upVotingUserUsername;
+    private String upVotingUserProfileImage;
+    private String postTitle;
+    private String postTimestamp;
+    private String postPoints;
+    private String communityName;
+}
