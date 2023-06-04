fb75dce636436dd00d6ab24bc4a32971fac290ab
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-06-04 23:34:14 +0200

    Delete the @NoArgsConstructor annotation from the CommentReplyEmail class

--- src/main/java/com/socaly/email/CommentReplyEmail.java
+++ src/main/java/com/socaly/email/CommentReplyEmail.java
@@ -2,11 +2,9 @@ package com.socaly.email;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class CommentReplyEmail {
     private String subject;
     private String recipientEmail;
