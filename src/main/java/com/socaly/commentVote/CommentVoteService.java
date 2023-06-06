aed918dd02c6983ef7f94afdacfd3c3dd9e4eb2d
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-06-06 23:09:58 +0200

    If the upvoted comment has a parent comment send a reply upvote email instead of a comment upvote email

--- src/main/java/com/socaly/commentVote/CommentVoteService.java
+++ src/main/java/com/socaly/commentVote/CommentVoteService.java
@@ -4,6 +4,7 @@ import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.socaly.comment.Comment;
 import com.socaly.email.CommentUpVoteEmail;
 import com.socaly.email.EmailService;
+import com.socaly.email.ReplyUpVoteEmail;
 import com.socaly.user.User;
 import com.socaly.util.VoteType;
 import com.socaly.comment.CommentNotFoundException;
@@ -101,25 +102,77 @@ public class CommentVoteService {
             commentReplyCountText = commentReplyCount + " replies";
         }
 
-        emailService.sendCommentUpVoteEmail(new CommentUpVoteEmail(
-                currentUser.getUsername() + " upvoted your comment on " + comment.getPost().getPostName()
-                        + " in s\\" + comment.getPost().getCommunity().getName(),
-                comment.getUser().getEmail(),
-                comment.getUser().getUsername(),
-                comment.getUser().getProfileImage().getImageUrl(),
-                comment.getPost().getCommunity().getName(),
-                comment.getPost().getUser().getUsername(),
-                TimeAgo.using(comment.getPost().getCreatedDate().toEpochMilli()),
-                comment.getPost().getPostName(),
-                postPointsText,
-                commentCountText,
-                TimeAgo.using(comment.getCreationDate().toEpochMilli()),
-                comment.getText(),
-                commentPointsText,
-                commentReplyCountText,
-                currentUser.getUsername(),
-                currentUser.getProfileImage().getImageUrl()
-        ));
+        if (comment.getParentCommentId() != null) {
+            Comment parentComment = commentRepository.findById(comment.getParentCommentId())
+                    .stream()
+                    .findFirst()
+                    .orElseThrow(
+                            () -> new CommentNotFoundException(comment.getParentCommentId().toString())
+                    );
+
+            int parentCommentPoints = parentComment.getVoteCount();
+            String parentCommentPointsText;
+
+            int parentCommentReplyCount = commentRepository.findByParentCommentId(parentComment.getId()).size();
+            String parentCommentReplyCountText;
+
+            if (parentCommentPoints == 1 || parentCommentPoints == -1) {
+                parentCommentPointsText = "1 point";
+            } else {
+                parentCommentPointsText = parentCommentPoints + " points";
+            }
+
+            if (parentCommentReplyCount == 1) {
+                parentCommentReplyCountText = "1 reply";
+            } else {
+                parentCommentReplyCountText = parentCommentReplyCount + " replies";
+            }
+
+            emailService.sendReplyUpVoteEmail(new ReplyUpVoteEmail(
+                    currentUser.getUsername() + " upvoted your reply on " + comment.getPost().getPostName()
+                            + " in s\\" + comment.getPost().getCommunity().getName(),
+                    comment.getUser().getEmail(),
+                    comment.getUser().getUsername(),
+                    comment.getUser().getProfileImage().getImageUrl(),
+                    comment.getPost().getCommunity().getName(),
+                    comment.getPost().getUser().getUsername(),
+                    TimeAgo.using(comment.getPost().getCreatedDate().toEpochMilli()),
+                    comment.getPost().getPostName(),
+                    postPointsText,
+                    commentCountText,
+                    parentComment.getUser().getUsername(),
+                    TimeAgo.using(parentComment.getCreationDate().toEpochMilli()),
+                    parentComment.getText(),
+                    parentCommentPointsText,
+                    parentCommentReplyCountText,
+                    TimeAgo.using(comment.getCreationDate().toEpochMilli()),
+                    comment.getText(),
+                    commentPointsText,
+                    commentReplyCountText,
+                    currentUser.getUsername(),
+                    currentUser.getProfileImage().getImageUrl()
+            ));
+        } else {
+            emailService.sendCommentUpVoteEmail(new CommentUpVoteEmail(
+                    currentUser.getUsername() + " upvoted your comment on " + comment.getPost().getPostName()
+                            + " in s\\" + comment.getPost().getCommunity().getName(),
+                    comment.getUser().getEmail(),
+                    comment.getUser().getUsername(),
+                    comment.getUser().getProfileImage().getImageUrl(),
+                    comment.getPost().getCommunity().getName(),
+                    comment.getPost().getUser().getUsername(),
+                    TimeAgo.using(comment.getPost().getCreatedDate().toEpochMilli()),
+                    comment.getPost().getPostName(),
+                    postPointsText,
+                    commentCountText,
+                    TimeAgo.using(comment.getCreationDate().toEpochMilli()),
+                    comment.getText(),
+                    commentPointsText,
+                    commentReplyCountText,
+                    currentUser.getUsername(),
+                    currentUser.getProfileImage().getImageUrl()
+            ));
+        }
     }
 
     private CommentVote mapToCommentVote(CommentVoteDto commentVoteDto, Comment comment) {
