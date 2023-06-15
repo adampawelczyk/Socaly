da6fd1e9dfaa6760f5929d5a7967ceb6a90707f3
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-06-15 21:35:06 +0200

    Rename the postTitle field to title

--- src/main/java/com/socaly/post/PostResponse.java
+++ src/main/java/com/socaly/post/PostResponse.java
@@ -11,7 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 public class PostResponse {
     private Long id;
-    private String postTitle;
+    private String title;
     private String description;
     private List<String> images;
     private String username;
