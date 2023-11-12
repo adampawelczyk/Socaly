018b1fd97c55b8715ac966a57884b9ce48ae8b7b
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-11-12 21:30:57 +0100

    Changed the signature of the 'logIn' method by marking the parameter as final

--- src/main/java/com/socaly/auth/AuthService.java
+++ src/main/java/com/socaly/auth/AuthService.java
@@ -146,7 +146,7 @@ public class AuthService {
         userRepository.save(user);
     }
 
-    public AuthResponse logIn(LogInRequest loginRequest) {
+    public AuthResponse logIn(final LogInRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginRequest.getUsername(), loginRequest.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
