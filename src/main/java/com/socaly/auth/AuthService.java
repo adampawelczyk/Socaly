741b7f5a06fef4761f959c531109926bcfa8e53a
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-11-26 23:59:19 +0100

    Deleted the 'public' access modifier from the 'encodePassword' method

--- src/main/java/com/socaly/auth/AuthService.java
+++ src/main/java/com/socaly/auth/AuthService.java
@@ -164,7 +164,7 @@ public class AuthService {
         return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password)).isAuthenticated();
     }
 
-    public String encodePassword(final String password) {
+    String encodePassword(final String password) {
         return passwordEncoder.encode(password);
     }
 
