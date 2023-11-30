5dc8b3322c19a42fbbdf4e32989e9750b07e4f38
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-11-30 23:55:20 +0100

    Renamed the 'savePostVote' method to 'vote'

--- src/main/java/com/socaly/postVote/PostVoteController.java
+++ src/main/java/com/socaly/postVote/PostVoteController.java
@@ -16,7 +16,7 @@ public class PostVoteController {
 
     @PostMapping
     public ResponseEntity<Void> vote(@RequestBody PostVoteDto postVoteDto) {
-        postVoteService.savePostVote(postVoteDto);
+        postVoteService.vote(postVoteDto);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
