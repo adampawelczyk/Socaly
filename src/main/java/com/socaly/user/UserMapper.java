e7cf0caa9f685acae942762759670fa2d4e9e7f0
Adam Pawelczyk <pawelczyk-adam@outlook.com>
2023-12-02 16:28:47 +0100

    Deleted the unused 'map' method

--- src/main/java/com/socaly/user/UserMapper.java
+++ src/main/java/com/socaly/user/UserMapper.java
@@ -1,16 +1,11 @@
 package com.socaly.user;
 
 import com.socaly.image.Image;
-import com.socaly.userSettings.UserSettingsResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class UserMapper {
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "profileImage", expression = "java(mapStringToImage(userResponse.getProfileImage()))")
-    public abstract User map(UserResponse userResponse);
 
     @Mapping(target = "profileImage", expression = "java(mapImageToString(user.getProfileImage()))")
     public abstract UserResponse mapToDto(User user);
